86a1bd9830735b0d9e2b056c64283a589bf94c91
ckyy1g20 <ckyy1g20@soton.ac.uk>
2021-07-12 11:37:52 +0100

    Allow keys to be unbound in InputManager

--- SDL 2D Game Engine/InputManager.cpp
+++ SDL 2D Game Engine/InputManager.cpp
@@ -35,3 +35,7 @@ bool InputManager::getCommand(Command c) {
 void InputManager::setKeyCommand(SDL_Keycode key, Command command) {
 	key_to_command[key] = command;
 }
+
+void InputManager::unbindKey(SDL_Keycode key) {
+	key_to_command.erase(key);
+}
